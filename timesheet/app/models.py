1767ca64266eaec18f296501dc3a5872a7c57ec3
Henrique Sebastião <contato@henriquesebastiao.com>
2023-12-26 23:17:46 -0400

    🐛 fix: Add date manually

--- timesheet/app/models.py
+++ timesheet/app/models.py
@@ -1,5 +1,7 @@
 """This module contains the models of the app."""
 
+import datetime
+
 from django.contrib.auth import get_user_model
 from django.db import models
 
@@ -8,7 +10,7 @@ class PointRecord(models.Model):
     """Model that represents a point record."""
 
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    register_date = models.DateField(auto_now_add=True)
+    register_date = models.DateField(default=datetime.date.today)
     clock_in_morning = models.TimeField(null=True, blank=True)
     clock_out_morning = models.TimeField(null=True, blank=True)
     clock_in_afternoon = models.TimeField(null=True, blank=True)
